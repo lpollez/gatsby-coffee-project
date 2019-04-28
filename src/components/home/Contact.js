b080d9c7470a59cdcb90e8ab0340aeceea9eff8b
Laurent Pollez <laurent.pollez@gmail.com>
2019-04-28 11:08:58 +0200

    Test form avec Netlify

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -13,6 +13,7 @@ export default function Contact() {
             data-netlify="true"
             data-netlify-honeypot="bot-field"
           >
+            <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="contact" />
             <div className="form-group">
               <label htmlFor="name">Nom</label>
