1619d5ebdec1e806b38b2ce286d382aa98c74083
Laurent Pollez <laurent.pollez@gmail.com>
2019-04-29 18:55:02 +0200

    Modification composant Info

--- src/components/home/Info.js
+++ src/components/home/Info.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import Title from "../globals/Title"
 
 export default function Info() {
@@ -9,7 +8,7 @@ export default function Info() {
         <Title title="Notre histoire" />
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
-            <p className="lead text-muted mb-5">
+            <p className="lead text-muted">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Voluptatum sequi possimus perferendis necessitatibus laborum ut,
               laudantium porro eaque, adipisci nisi reprehenderit deserunt
@@ -18,11 +17,6 @@ export default function Info() {
               repellendus debitis ut temporibus, laborum et inventore fuga esse,
               pariatur omnis rem cumque?
             </p>
-            <Link to="/about/">
-              <button className="btn text-uppercase btn-yellow">
-                Qui sommes-nous
-              </button>
-            </Link>
           </div>
         </div>
       </div>
