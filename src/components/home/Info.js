ecf441a4fcd1129dd0018767141efb92cbd61f3d
Laurent Pollez <laurent.pollez@gmail.com>
2019-05-09 22:42:35 +0200

    Modification message Info

--- src/components/home/Info.js
+++ src/components/home/Info.js
@@ -19,14 +19,17 @@ export default function Info({ title, message }) {
 Info.defaultProps = {
   title: "Infos",
   message: `Ceci est un simple exemple de site marchand. Il est développé
-              avec les technologies modernes (notamment React qui est la
-              librairie de programmation développée par Facebook), ce qui le
-              rend très performant. A contenu égal, il est 6 à 10 fois plus
-              rapide qu'un site développé avec un CMS standard comme Wordpress
-              ou Drupal. De plus, cette technologie offre une sécurité optimale
-              et un hébergement simplifié. Ce site de démonstration intègre un
-              formulaire de contact, un filtre sur produits et un panier
-              d'achats. Vous pouvez tester le processus d'achats avec les
-              données de tests indiquées. Bien sûr, le site est responsive, càd
-              que son contenu s'adapte à la taille de l'écran.`,
+            avec les technologies modernes (notamment React qui est la
+            librairie de programmation développée par Facebook), ce qui le
+            rend très performant. A contenu égal, il est 6 à 10 fois plus
+            rapide qu'un site développé avec un CMS standard comme Wordpress
+            ou Drupal. De plus, cette technologie offre une sécurité optimale
+            et un hébergement simplifié. Ce site de démonstration intègre un
+            formulaire de contact, un filtre sur produits et un panier
+            d'achats. Vous pouvez tester le processus d'achats avec les
+            données de tests indiquées. Bien sûr, le site est responsive, càd
+            que son contenu s'adapte à la taille de l'écran. Les
+            possibilités de développement sont extrêmement puissantes et
+            nombreuses (site vitrine, site e-commerce, applications métiers,
+            progressive web apps, ...).`,
 }
